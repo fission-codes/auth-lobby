369f9b591b4275a310a031e4a5aec3e25062b95f
Daniel Holmgren <dtholmgren@gmail.com>
2020-03-03 18:34:31 -0600

    static assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
-import ParentApp from './ParentApp'
 import ChildApp from './ChildApp'
 
 class App extends React.Component {
   render() {
-    // Placeholder
-    // These will be on different domains
-    const path = window.location.pathname
-    if(path === '/login'){
-      return <ChildApp />
-    }else{
-      return <ParentApp />
-    }
+    return <ChildApp />
   }
 }
 
